9e2ee77c27cd4014f466c4ef3b0f780f513fdd40
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2016-05-14 17:49:52 +0200

    bug fix

--- app/src/main/java/com/laxen/auxiliaire/tabs/ProfileFragment.java
+++ app/src/main/java/com/laxen/auxiliaire/tabs/ProfileFragment.java
@@ -14,7 +14,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_user,container,false);
 
         return view;
     }
