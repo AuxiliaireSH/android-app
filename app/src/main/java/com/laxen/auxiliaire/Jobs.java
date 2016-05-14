73474abd8cecf55fb2e4946c59a492c504ba7372
Johan Andersson <johandi@student.chalmers.se>
2016-05-14 11:15:28 +0200

    Add List object of jobs

---
+++ app/src/main/java/com/laxen/auxiliaire/Jobs.java
@@ -0,0 +1,16 @@
+package com.laxen.auxiliaire;
+
+import java.util.List;
+
+public class Jobs {
+
+	private List<Job> jobs;
+
+	public List<Job> getJobs() {
+		return jobs;
+	}
+
+	public void setJobs(List<Job> jobs) {
+		this.jobs = jobs;
+	}
+}
